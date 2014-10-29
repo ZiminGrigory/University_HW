30569b99420e0016620713cf3eed3aed63d951b7
lahmatbiy <zimin_grigory@hotmail.com>
2014-10-29 18:25:47 +0300

    change file location + upd task3

--- graphics/task2/task2Text.py
+++ graphics/task2/task2Text.py
@@ -1,17 +1,18 @@
 import cv2
 
+
 def main():
-    img = cv2.imread("text.bmp", cv2.CV_LOAD_IMAGE_COLOR)  ## Read image file
+    img = cv2.imread("text.bmp", cv2.CV_LOAD_IMAGE_COLOR)  # Read image file
     img = cv2.GaussianBlur(img, (3, 3), 0)
     img = cv2.Laplacian(img, 0)
     cv2.threshold(img, 50, 255, cv2.THRESH_BINARY, img)
     cv2.imwrite("output.bmp", img)
 
-    cv2.namedWindow('Display Window', cv2.WINDOW_NORMAL)        ## create window for display
-    cv2.imshow('Display Window',img)         ## Show image in the window
+    cv2.namedWindow('Display Window', cv2.WINDOW_NORMAL)        # Create window for display
+    cv2.imshow('Display Window', img)         # Show image in the window
     cv2.moveWindow('Display Window', 1, 1)
-    print ("size of image: ",img.shape)        ## print size of image
-    cv2.waitKey(0)                           ## Wait for keystroke
-    cv2.destroyAllWindows()                  ## Destroy all windows
+    print ("size of image: ", img.shape)      # print size of image
+    cv2.waitKey(0)                           # Wait for keystroke
+    cv2.destroyAllWindows()                  # Destroy all windows
 
 main()
--- graphics/task3/task3Text.py
+++ graphics/task3/task3Text.py
@@ -1,6 +1,5 @@
 __author__ = 'home'
 import cv2
-import cv
 import numpy as np
 
 
@@ -15,23 +14,22 @@ def main():
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     contours, hierarchy = cv2.findContours(gray, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 
-
     color = (255, 255, 255)
     drawing = np.zeros(img.shape, np.uint8)
     for cnt in contours:
-        cv2.drawContours(drawing, [cnt], 0, color, 0)
-
+        x, y, w, h = cv2.boundingRect(cnt)
+        cv2.rectangle(drawing, (x, y), (x + w, y + h), color)
 
     drawing = cv2.cvtColor(drawing, cv2.COLOR_RGB2GRAY)
     h, w = img.shape[:2]
-    outp = np.zeros((h-2, w-2), np.uint8)
-    cv2.floodFill(outp, drawing, (0, 0), color)
+    outpImg = np.zeros((h-2, w-2), np.uint8)
+    cv2.floodFill(outpImg, drawing, (0, 0), color)
 
-    cv2.threshold(outp, 254, 255, cv2.THRESH_BINARY_INV, outp)
+    cv2.threshold(outpImg, 254, 255, cv2.THRESH_BINARY_INV, outpImg)
 
-    cv2.imwrite("outputTask3.jpg", outp)
+    cv2.imwrite("outputTask3.jpg", outpImg)
     cv2.namedWindow('Display Window', cv2.WINDOW_NORMAL)        # Create window for display
-    cv2.imshow('Display Window', outp)         # Show image in the window
+    cv2.imshow('Display Window', outpImg)         # Show image in the window
     cv2.moveWindow('Display Window', 1, 1)
     print ("size of image: ", img.shape)      # print size of image
     cv2.waitKey(0)                           # Wait for keystroke
