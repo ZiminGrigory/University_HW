6f28194b344c2a9e016b241cd2d09940b5c6de16
lahmatbiy <zimin_grigory@hotmail.com>
2015-03-04 17:51:31 +0300

    fixed

--- c++_course/AdditionalTask2/main.cpp
+++ c++_course/AdditionalTask2/main.cpp
@@ -10,7 +10,7 @@ int getNearest(const int* const array, int n, const int number)
 		if (a[i] <= number)
 			i += (n-i)/2;
 		else
-			i -= (n-i)/2;
+			i = i/2;
 	return abs(a[i] - number) <= abs(a[i+1] - number) ? a[i]: a[i+1];
 }
 
@@ -43,20 +43,23 @@ bool compare(const char* const s1, const char* const s2)
 
 int main()
 {
-	const int a[10] {-3, -2, 0, 1, 3, 5, 7, 10, 20, 23};
-	cout << (getNearest(a, 10, -3) == -3) << "\n";
-	cout << (getNearest(a, 10, 2) == 1) << "\n";
-	cout << (getNearest(a, 10, 22) == 23) << "\n";
-	cout << (getNearest(a, 10, 25) == 23) << "\n";
-	cout << (getNearest(a, 10, -233) == -3) << "\n";
+//	const int a[10] {-3, -2, 0, 1, 3, 5, 7, 10, 20, 23};
+//	cout << (getNearest(a, 10, -3) == -3) << "\n";
+//	cout << (getNearest(a, 10, 2) == 1) << "\n";
+//	cout << (getNearest(a, 10, 22) == 23) << "\n";
+//	cout << (getNearest(a, 10, 25) == 23) << "\n";
+//	cout << (getNearest(a, 10, -233) == -3) << "\n";
 
-	//
-	char s1[100];
-	char s2[100];
-	cout << "enter couple lines pls\n";
-	cin.getline(s1, 100);
-	cin.getline(s2, 100);
-	cout << " compare2 s1 s2 = " << compare(s1, s2) << endl;
+//	//
+//	char s1[100];
+//	char s2[100];
+//	cout << "enter couple lines pls\n";
+//	cin.getline(s1, 100);
+//	cin.getline(s2, 100);
+//	cout << " compare2 s1 s2 = " << compare(s1, s2) << endl;
+
+	const int a[10] {-4, -2, 0, 1, 3, 5, 7, 10};
+	cout << (getNearest(a, 8, -3) == -4) <<  "\n" ;
 
 	return 0;
 }
